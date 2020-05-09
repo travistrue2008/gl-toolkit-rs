79e5ab9db01737f665e65095a731c5b89af7d997
travistrue2008 <travis.true08@gmail.com>
2020-05-08 18:21:11 -0700

    Fixed whitespaces

--- src/builtin.rs
+++ src/builtin.rs
@@ -3,79 +3,79 @@ use crate::shader::{Shader, Stage, StageKind};
 use lazy_static::lazy_static;
 
 const SRC_BASIC_VERTEX: &str = r#"
-	#version 330 core
+    #version 330 core
 
-	layout (location = 0) in vec2 a_pos;
+    layout (location = 0) in vec2 a_pos;
 
-	void main() {
-		gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
-	}
+    void main() {
+        gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
+    }
 "#;
 
 const SRC_BASIC_FRAGMENT: &str = r#"
-  #version 330 core
+    #version 330 core
 
-  uniform vec4 u_color;
+    uniform vec4 u_color;
 
-  out vec4 out_color;
+    out vec4 out_color;
 
-  void main() {
-	out_color = u_color;
-  }
+    void main() {
+        out_color = u_color;
+    }
 "#;
 
 const SRC_COLOR_VERTEX: &str = r#"
-	#version 330 core
+    #version 330 core
 
-	layout (location = 0) in vec2 a_pos;
-	layout (location = 1) in vec4 a_color;
+    layout (location = 0) in vec2 a_pos;
+    layout (location = 1) in vec4 a_color;
 
-	out vec4 v_color;
+    out vec4 v_color;
 
-	void main() {
-		v_color = a_color;
-		gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
-	}
+    void main() {
+        v_color = a_color;
+        gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
+    }
 "#;
 
 const SRC_COLOR_FRAGMENT: &str = r#"
-  #version 330 core
+    #version 330 core
 
-  in vec4 v_color;
+    in vec4 v_color;
 
-  out vec4 out_color;
+    out vec4 out_color;
 
-  void main() {
-	out_color = v_color;
-  }
+    void main() {
+        out_color = v_color;
+    }
 "#;
 
 const SRC_TEXTURE_VERTEX: &str = r#"
-	#version 330 core
+    #version 330 core
 
-	layout (location = 0) in vec2 a_pos;
-	layout (location = 1) in vec2 a_coord;
+    layout (location = 0) in vec2 a_pos;
+    layout (location = 1) in vec2 a_coord;
 
-	out vec2 v_coord;
+    out vec2 v_coord;
 
-	void main() {
-		v_coord = a_coord;
-		gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
-	}
+    void main() {
+        v_coord = a_coord;
+        gl_Position = vec4(a_pos.x, a_pos.y, 0.0, 1.0);
+    }
 "#;
 
 const SRC_TEXTURE_FRAGMENT: &str = r#"
-  #version 330 core
+    #version 330 core
 
-  uniform sampler2D u_tex;
+    uniform sampler2D u_tex;
 
-  in vec2 v_coord;
+    in vec2 v_coord;
 
-  out vec4 out_color;
+    out vec4 out_color;
 
-  void main() {
-	out_color = texture(u_tex, v_coord);
-  }
+    void main() {
+        out_color = texture(u_tex, v_coord);
+    }
 "#;
 
 lazy_static! {
